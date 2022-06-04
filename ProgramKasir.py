46bed1813991ff72e62e64df669f2ba5a724b2f2
carinaaaflo <106786143+carinaaaflo@users.noreply.github.com>
2022-06-04 15:36:56 +0700

    Add files via upload

--- ProgramKasir.py
+++ ProgramKasir.py
@@ -5,23 +5,23 @@ st.write("-------------------------- Selamat Datang Di Chilless Cafe -----------
 
 st.write("====================== Daftar Menu ======================")
 
-st.write("1. Chicken Ball  :           @23000")
-st.write("2. French Fries  :           @15000")
-st.write("3. Vanila Latte  :           @18000")
-st.write("4. Rose Tea      :           @15000")
-st.write("5. Dumpling      :           @12000")
-st.write("6. Ice Cream     :           @10000")
-st.write("7. Pecel Buaya Darat :       @200000")
-st.write("8. Nasi Goreng Cacing Kremi :@150000") 
+st.write("1. Chicken Ball  :           23000")
+st.write("2. French Fries  :           15000")
+st.write("3. Vanila Latte  :           18000")
+st.write("4. Rose Tea      :           15000")
+st.write("5. Dumpling      :           12000")
+st.write("6. Ice Cream     :           10000")
+st.write("7. Pecel Buaya Darat :       200000")
+st.write("8. Nasi Goreng Cacing Kremi :150000") 
 
 st.write("====================== Daftar Menu ======================")
 
     
 
 nama = st.text_input('Nama pelanggan')
-beli = int(st.number_input("Pilih Menu (nomor booking) :"))
+beli = int(st.number_input("Pilih Menu (harga menu) :"))
 jumlah =int(st.number_input("Jumlah Pesanan :"))
-totalharga = jumlah*beli
+totalharga = jumlah * beli
 
 
 st.write("====================== Detail Pembayaran ======================")
